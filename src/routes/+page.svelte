937036cd5cda45450192b44dc68edec5b84bf6a2
janealsh <janealshihabi@gmail.com>
2026-01-22 17:56:19 -0500

    add SvelteFlow library and use to configure +page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { SvelteFlow, Background } from "@xyflow/svelte";
+    import '@xyflow/svelte/dist/style.css';
+</script>
+
+<div style:width="100vw" style:height="100vh">
+    <SvelteFlow>
+        <Background/>
+    </SvelteFlow>
+</div>
