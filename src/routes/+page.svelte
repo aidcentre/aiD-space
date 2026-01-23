993e75a8dfee6d38395955125640e56eb2f26ed0
janealsh <janealshihabi@gmail.com>
2026-01-23 14:51:19 -0500

    fix background and add page title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,20 @@
 <script>
-    import { SvelteFlow, Background } from "@xyflow/svelte";
-    import '@xyflow/svelte/dist/style.css';
+	import { SvelteFlow, Background, BackgroundVariant } from '@xyflow/svelte';
+	import '@xyflow/svelte/dist/style.css';
 </script>
 
+<svelte:head>
+	<title>AID Home</title>
+</svelte:head>
+
 <div style:width="100vw" style:height="100vh">
-    <SvelteFlow>
-        <Background/>
-    </SvelteFlow>
+	<SvelteFlow maxZoom={1} minZoom={1}>
+		<!-- can add patternClass -->
+		<Background
+			bgColor="#0A0A0A"
+			patternColor="#d9d9d9"
+			variant={BackgroundVariant.Cross}
+			gap={40}
+		/>
+	</SvelteFlow>
 </div>
