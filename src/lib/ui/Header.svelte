8d430d98ff6b8232fb43c3bed78f843118a25379
janealsh <janealshihabi@gmail.com>
2026-02-02 13:44:40 +0100

    improve header formatting

--- src/lib/ui/Header.svelte
+++ src/lib/ui/Header.svelte
@@ -4,7 +4,7 @@
 	let { lastCommit, author } = $props();
 </script>
 
-<div class="header navbar text-gray-200 shadow-sm">
+<div class="header navbar text-gray-200 shadow-sm items-start pt-4">
 	<div class="navbar-start pl-3 text-xs">
 		<span>v0.1</span>
 	</div>
