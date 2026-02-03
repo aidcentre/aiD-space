aaeb59a1016b753c01ff360abbfb62fca40d5ae5
janealsh <janealshihabi@gmail.com>
2026-02-03 15:23:56 +0100

    add IMAGE node and temporary source material (image w/ logo)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,11 @@
 	import TextNode from '$lib/ui/TextNode.svelte';
 	import Header from '$lib/ui/Header.svelte';
 	import '@xyflow/svelte/dist/style.css';
+	import ImgNode from '$lib/ui/imgNode.svelte';
 
 	let nodeTypes = {
-		textNode: TextNode
+		textNode: TextNode,
+		imageNode: ImgNode
 	};
 
 	let nodes = $state.raw([
@@ -13,15 +15,28 @@
 			id: '1',
 			width: 400,
 			draggable: false,
-			data: { text: 'aiD is building a scientific and technological foundation that makes reliable, real-world AI possible.' },
+			data: {
+				text: 'aiD is building a scientific and technological foundation that makes reliable, real-world AI possible.'
+			},
 			type: 'textNode',
 			position: { x: 0, y: 0 }
+		},
+		{
+			id: '3',
+			width: 400,
+			draggable: false,
+			data: {
+				text: 'THIS IS AN IMAGE NODE!',
+				fileName: 'image'
+			},
+			type: 'imageNode',
+			position: { x: 0, y: 300 }
 		},
 		{
 			id: '5',
 			width: 300,
 			draggable: false,
-			data: { text: 'We\'re building something great here. Watch this space.' },
+			data: { text: "We're building something great here. Watch this space." },
 			type: 'textNode',
 			position: { x: 900, y: 20 }
 		}
@@ -35,7 +50,14 @@
 <Header lastCommit="2 days" author="CamillaS" />
 
 <div class="sf-bg">
-	<SvelteFlow bind:nodes {nodeTypes} minZoom={1} maxZoom={1} fitView  proOptions={{ hideAttribution: true }}>
+	<SvelteFlow
+		bind:nodes
+		{nodeTypes}
+		minZoom={1}
+		maxZoom={1}
+		fitView
+		proOptions={{ hideAttribution: true }}
+	>
 		<Background
 			id="1"
 			bgColor="#050505"
