ea6bd6a7195f7a6ad00eda20beb4a11f4146883d
janealsh <janealshihabi@gmail.com>
2026-02-09 17:14:38 +0100

    make preliminary tornado navigation animation

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import NavHeader from '$lib/ui/NavHeader.svelte';
 	import WhatIsAid from '$lib/about/AboutAidHero.svelte';
+	import Tornado from '$lib/ui/Tornado.svelte';
 </script>
 
 <NavHeader version={0.1} nodeNum={2} />
 
 <main>
+	<Tornado />
 	<WhatIsAid />
 </main>
 
