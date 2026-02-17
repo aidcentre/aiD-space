6e9984084dcf19891ce8576788ace796683150fc
janealsh <janealshihabi@gmail.com>
2026-02-17 17:07:50 +0100

    fix contact page

---
+++ src/lib/contact/ContactInfoHero.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+</script>
+
+<section class="mt-20 flex w-full flex-col items-center justify-center">
+	<div class="flex w-1/2 flex-col justify-start gap-8">
+		<p class="text-4xl font-extrabold">About & contact</p>
+		<p class="font-[Montagu_Slab] text-lg font-normal tracking-tight">
+			aiD is headquartered at the Norwegian University of Science and Technology in Trondheim,
+			Norway and at SINTEF, Norway.
+		</p>
+		<div class="flex flex-col">
+			<span class="text-sm font-bold">info@aid-research.com</span>
+			<span class="=mt-4 text-sm font-bold">+47 22 22 55 55</span>
+		</div>
+	</div>
+</section>
+
+<style>
+</style>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import NavHeader from '$lib/ui/NavHeader.svelte';
+	import ContactInfoHero from '$lib/contact/ContactInfoHero.svelte';
 </script>
 
 <NavHeader version={0.1} nodeNum={4} />
 
 <main>
-	<h2>About and contact</h2>
-	<p>aid is headquartere blah blah</p>
+	<ContactInfoHero />
 </main>
