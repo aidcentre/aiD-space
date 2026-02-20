5a9f925d6983d585b43dec678c96f9cd5821e3df
janealsh <janealshihabi@gmail.com>
2026-02-20 14:10:19 +0100

    incorporate scrolltrigger plugin to make animation run rwhen user scrolls to <Scramble />

--- src/lib/actions/Scramble.svelte
+++ src/lib/actions/Scramble.svelte
@@ -1,21 +1,28 @@
 <script lang="ts">
 	import { gsap } from 'gsap';
 	import { ScrambleTextPlugin } from 'gsap/ScrambleTextPlugin';
+	import { ScrollTrigger } from 'gsap/ScrollTrigger';
 	import { onMount } from 'svelte';
-	// const { text = '', class: className } = $props();
 	const { text, class: className } = $props();
 
-	gsap.registerPlugin(ScrambleTextPlugin);
+	// scrolltrigger plugin makes animation run when user scrolls to it
+	gsap.registerPlugin(ScrambleTextPlugin, ScrollTrigger);
 
 	let element: HTMLElement;
 
 	onMount(() => {
 		gsap.to(element, {
-			duration: 1,
+			duration: 1.5,
 			scrambleText: {
 				text: text,
 				chars: '0123456789',
 				speed: 0.5
+			},
+
+			scrollTrigger: {
+				trigger: element,
+				start: 'bottom 75%',
+				toggleActions: 'restart none none none'
 			}
 		});
 	});
