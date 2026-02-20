c732a3bdc4211bfa1175731e6c536da58318ae7b
janealsh <janealshihabi@gmail.com>
2026-02-20 13:12:28 +0100

    add text scramble component and utilize in the /about page

---
+++ src/lib/actions/Scramble.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { gsap } from 'gsap';
+	import { ScrambleTextPlugin } from 'gsap/ScrambleTextPlugin';
+	import { onMount } from 'svelte';
+	// const { text = '', class: className } = $props();
+	const { text, class: className } = $props();
+
+	gsap.registerPlugin(ScrambleTextPlugin);
+
+	let element: HTMLElement;
+
+	onMount(() => {
+		gsap.to(element, {
+			duration: 1,
+			scrambleText: {
+				text: text,
+				chars: '0123456789',
+				speed: 0.5
+			}
+		});
+	});
+</script>
+
+<span bind:this={element} class={className}>
+	{text}
+</span>
