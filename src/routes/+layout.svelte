6d4632d27803c3ff3a7aff4a29bb6b556c193e8b
janealsh <janealshihabi@gmail.com>
2026-02-23 17:14:33 +0100

    set max width wrapper for page content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,10 +7,16 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-{@render children()}
+<main class="page-layout">
+	{@render children()}
+</main>
 
 <style>
 	:global(html) {
 		background-color: var(--light-grey);
+	}
+	.page-layout {
+		max-width: 1920px;
+		margin: 0 auto;
 	}
 </style>
