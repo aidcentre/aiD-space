9f25bf2f021e8e8f863c4e83eccb21b326e5c287
janealsh <janealshihabi@gmail.com>
2026-02-24 15:39:25 +0100

    fix styling of home footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
 <main class="pointer-events-none relative flex min-h-dvh flex-col items-center">
 	<HomeHeader />
 	<SearchBar />
-	<footer class="w-2/3 self-start px-3 pb-3">
+	<footer class="w-2/3 self-start px-3 pb-3 text-[1.5rem] leading-8 font-extrabold text-off-black">
 		The Norwegian Center on AI for Decisions is building a scientific and technological foundation
 		that makes reliable, real-world AI possible. We're building something great here. Watch this
 		space.
