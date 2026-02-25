5f87acd64ec3c02d40f0057685d39c6946af1ea6
janealsh <janealshihabi@gmail.com>
2026-02-25 14:03:18 +0100

    use custom layout for header

--- src/lib/ui/HomeHeader.svelte
+++ src/lib/ui/HomeHeader.svelte
@@ -4,8 +4,8 @@
 	import aidLogo from '/src/lib/assets/aiD_logotype_black_RGB.svg';
 </script>
 
-<div class="navbar items-start pt-4" style:color="var(--light-grey)">
-	<div class="navbar-start pl-3 text-xs">
+<div class=" relative flex min-h-16 w-full items-center justify-between p-2 pt-4">
+	<div class="inline-flex w-fit items-center justify-start pl-3 text-xs">
 		<!-- TODO: consider using <object> instead for accessibility -->
 		<img
 			src={aidLogo}
@@ -18,7 +18,7 @@
 			class="max-h-[30px] max-sm:hidden"
 		/>
 	</div>
-	<div class="pointer-events-auto mr-2 navbar-end gap-2 text-xs">
+	<div class="pointer-events-auto mr-2 inline-flex w-fit items-center justify-end gap-2 text-xs">
 		<SmallButton label="What is aiD?" link="about" colourMode="light" />
 		<SmallButton label="Contact" link="contact" colourMode="light" />
 	</div>
