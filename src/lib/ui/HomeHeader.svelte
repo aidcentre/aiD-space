b1069bdd4b146229158ba7e6147a90d75659277c
janealsh <janealshihabi@gmail.com>
2026-02-25 13:44:44 +0100

    add new logotype and fix lockup's size

--- src/lib/ui/HomeHeader.svelte
+++ src/lib/ui/HomeHeader.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
 	import SmallButton from './SmallButton.svelte';
-	import aiDLogo from '/src/lib/assets/aiD_lockup_black_RGB.svg';
+	import aidLogoWithText from '/src/lib/assets/aiD_lockup_black_RGB.svg';
+	import aidLogo from '/src/lib/assets/aiD_logotype_black_RGB.svg';
 </script>
 
 <div class="navbar items-start pt-4" style:color="var(--light-grey)">
 	<div class="navbar-start pl-3 text-xs">
 		<!-- TODO: consider using <object> instead for accessibility -->
-
 		<img
-			src={aiDLogo}
+			src={aidLogoWithText}
 			alt="aiD logo and description, Norwegian Centre on AI for Decisions"
-			width="200rem"
-			height="200rem"
+			class="max-h-[30px]"
 		/>
 	</div>
 	<div class="pointer-events-auto mr-2 navbar-end gap-2 text-xs">
