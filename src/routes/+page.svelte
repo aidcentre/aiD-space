4f9922af9c35c50b6eb97accb784695298546e66
janealsh <janealshihabi@gmail.com>
2026-02-26 09:14:49 +0100

    add new scrambletext function rather than component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,10 @@
 	import HomeHeader from '$lib/ui/HomeHeader.svelte';
 	import SearchBar from '$lib/ui/SearchBar.svelte';
 	import Tornado from '$lib/ui/Tornado.svelte';
+	import { animate } from '$lib/actions/scramble_text';
+
+	let footerText =
+		"The Norwegian Center on AI for Decisions is building a scientific and technological foundation that makes reliable, real-world AI possible. We\'re building something great here. Watch this space.";
 </script>
 
 <svelte:head>
@@ -15,10 +19,17 @@
 	<HomeHeader />
 	<SearchBar />
 	<footer
-		class="w-full self-start px-3 pb-3 text-[1rem] leading-6 font-extrabold text-off-black sm:w-2/3 sm:text-[1.5rem] sm:leading-8"
+		class="w-full self-start px-3 pb-3 text-[1rem] leading-6 font-extrabold wrap-anywhere text-off-black sm:w-2/3 sm:text-[1.5rem] sm:leading-8"
+		use:animate={{
+			type: 'to',
+			duration: 4,
+			scrambleText: {
+				text: footerText,
+				chars: '0123456789',
+				speed: 0.1
+			}
+		}}
 	>
-		The Norwegian Center on AI for Decisions is building a scientific and technological foundation
-		that makes reliable, real-world AI possible. We're building something great here. Watch this
-		space.
+		{footerText}
 	</footer>
 </main>
