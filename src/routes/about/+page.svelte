eebbd815251c0fa1bba891bb89a58f2f0cf14690
janealsh <janealshihabi@gmail.com>
2026-02-26 11:48:02 +0100

    add responsiveness to intro text in /about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -13,12 +13,17 @@
 <NavHeader version={0.1} nodeNum={2} />
 
 <main>
-	<p class=" ml-6 flex h-[46vh] w-6/11 items-center text-[1.2rem] font-extrabold">
-		The Norwegian Centre on AI for Decisions (aiD) is a premier research hub dedicated to advancing
-		the role of artificial intelligence in complex decision-making processes. As a cornerstone of
-		the Research Council of Norway's (RCN) AI portfolio, aiD bridges technological, organizational,
-		and human-centric gaps to foster a society where AI-driven value creation is safe and ethical.
-	</p>
+	<section class="flex h-[50vh] items-center px-2 sm:px-4">
+		<p
+			class="mr-1 text-[1rem] leading-6 font-bold text-off-black sm:ml-2 sm:w-54/100 sm:text-[1.5rem] sm:leading-8"
+		>
+			The Norwegian Centre on AI for Decisions (aiD) is a premier research hub dedicated to
+			advancing the role of artificial intelligence in complex decision-making processes. As a
+			cornerstone of the Research Council of Norway's (RCN) AI portfolio, aiD bridges technological,
+			organizational, and human-centric gaps to foster a society where AI-driven value creation is
+			safe and ethical.
+		</p>
+	</section>
 	<!-- <Tornado /> , uncomment when ready-->
 	<WhatIsAid />
 	<ResearchAreasHero />
