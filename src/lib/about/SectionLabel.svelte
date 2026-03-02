166b71538cbec899eee64fd455e6ce8285f05bab
janealsh <janealshihabi@gmail.com>
2026-03-02 16:37:43 +0100

    fix section label layout

--- src/lib/about/SectionLabel.svelte
+++ src/lib/about/SectionLabel.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-	class="flex size-fit rounded-[6px] bg-off-black px-4 py-3 text-[1rem] leading-[1.5rem] font-semibold text-nowrap text-white"
+	class="flex size-fit items-center rounded-[8px] bg-off-black px-4 py-3 text-[1rem] leading-[17.6px] font-semibold text-nowrap text-white"
 >
 	{label}
 </div>
