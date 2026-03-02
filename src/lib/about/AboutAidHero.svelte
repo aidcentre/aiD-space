c8b39f54f1cb3d4a42f24c5b3be7d8d791b04c8a
janealsh <janealshihabi@gmail.com>
2026-03-02 12:43:59 +0100

    change montagu slab font to 120pt

--- src/lib/about/AboutAidHero.svelte
+++ src/lib/about/AboutAidHero.svelte
@@ -11,20 +11,22 @@
 			<PartnerStats orgType="Research Organizations" orgAmount="6" />
 			<PartnerStats orgType="Professional Organizations (POs)" orgAmount="50" />
 		</div>
-		<div class="font-[Montagu_Slab] text-[1rem] leading-6 font-normal lg:columns-2 lg:gap-16">
-			<p class="mb-4">
+		<div
+			class="font-normAL font-[Montagu_Slab] text-[1rem] leading-6 lg:columns-2 lg:gap-16 lg:text-[1.44rem] lg:leading-[2rem]"
+		>
+			<p class="mb-[2rem]">
 				aiD is an interdisciplinary collaboration involving 3 academic institutions, 6 research
 				organizations, and over 50 professional organizations (POs). The center is led by Director
 				Prof. Sebastien Gros (NTNU) and Co-Director Dr. Signe Riemer-Sørensen (SINTEF Digital),
 				supported by a management team with responsibility for fundamental research,
 				operationalization, and innovation.
 			</p>
-			<p class="mb-4">
+			<p class="mb-[2rem]">
 				Our primary objective is to advance AI for decision-making through fundamental research and
 				real-world use cases, ensuring that AI-enhanced human decisions or fully autonomous systems
 				are effective, safe, and trustworthy in sectors critical to society.
 			</p>
-			<p class="mb-4">
+			<p class="mb-[2rem]">
 				aiD moves beyond traditional AI models that are purely descriptive. Instead, we employ a
 				holistic framework where the primary learning objective is the decision outcome itself. This
 				involves backpropagating learning through the entire decision process, from converting raw
