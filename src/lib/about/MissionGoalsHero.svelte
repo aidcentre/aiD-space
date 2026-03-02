e2c6743d66808261e419144974fa3286a6bd96d8
janealsh <janealshihabi@gmail.com>
2026-03-02 15:01:43 +0100

    make responsive

--- src/lib/about/MissionGoalsHero.svelte
+++ src/lib/about/MissionGoalsHero.svelte
@@ -8,27 +8,28 @@
 	];
 </script>
 
-<section class="grid grid-cols-1 gap-10 px-4 text-off-black xl:grid-cols-[1fr_5fr] xl:gap-0">
+<section class="grid grid-cols-1 gap-10 px-4 text-off-black lg:grid-cols-[1fr_5fr] lg:gap-0">
 	<SectionLabel label="Mission and Goals" />
 	<div class="flex flex-col gap-10">
 		<p
-			class="w-full font-[Montagu_Slab] leading-[1.5rem] font-[1rem] font-normal lg:text-[1.44rem] lg:leading-[2rem] xl:w-3/5"
+			class="w-full font-[Montagu_Slab] leading-[1.5rem] font-[1rem] font-normal lg:w-3/5 lg:pl-8 lg:text-[1.44rem] lg:leading-[2rem]"
 		>
 			A core mission of AID is to develop the next generation of AI experts. We aim to educate 400
 			MSc students and 30 PhDs/postdocs, while strengthening the expertise of over 50 existing
 			researchers. By pairing academic researchers with professional partners, we ensure that our
 			training is grounded in real-world industrial and societal needs.
 		</p>
-		<div class="flex flex-col gap-[1rem]">
+		<div class="flex flex-col gap-[1rem] lg:pl-8">
 			<p class="text-[1rem] leading-[1.5rem] font-bold text-(--grey)">Key milestones</p>
 			<div
-				class=" sm: flex flex-col items-start justify-center gap-[1rem] sm:w-9/10 sm:flex-row sm:justify-between"
+				class="flex flex-col items-start justify-center gap-[1rem] sm:flex sm:w-9/10 sm:flex-row sm:justify-between"
 			>
+				<!-- render each milestone -->
 				{#each milestones as ms}
 					<div class="flex items-center gap-4">
 						<div class="squircle"></div>
 						<p
-							class="font-[Milling] text-[1rem] leading-[1.5rem] font-bold sm:text-[1.5rem] sm:leading-[2rem]"
+							class="font-[Milling] text-[1rem] leading-[1.5rem] font-bold xl:text-[1.5rem] xl:leading-[2rem]"
 						>
 							{ms}
 						</p>
