c7acfb216a56ebbac03accd26e4ab9029f9d1e1d
janealsh <janealshihabi@gmail.com>
2026-03-03 14:19:30 +0100

    implement mobile version of use case section

--- src/lib/about/ImpactUseCaseHero.svelte
+++ src/lib/about/ImpactUseCaseHero.svelte
@@ -27,64 +27,45 @@
 	];
 </script>
 
-<section class="isolate grid grid-cols-2 gap-x-72 gap-y-24 bg-(--off-black) pt-16 pr-4 pb-16 pl-4">
-	<!-- TODO: change size of parent div (make it smaller) so that the dot bg ends earlier -->
-	<div class="dot-bg pointer-events-none sticky top-12 z-20 col-span-2 pt-8 text-white"></div>
-	<div
-		class="sticky top-9 z-10 col-span-2 -mt-32 h-40 w-full bg-linear-to-b from-black to-transparent"
-	></div>
-	<div class="title-area sticky top-40 z-15 -mt-32">
-		<a
-			class="section-title flex w-max text-[0.8rem] font-bold drop-shadow-none"
-			id="impact-and-use-cases"
-			href="#impact-and-use-cases">Real-World Impact and Use Cases</a
-		>
-		<h6 class="mb-6 w-full text-lg font-extrabold text-white">
-			<span
-				>aiD demonstrates its research progress through more than 10 high-impact use cases organized
-				into five topical areas and driven by industrial partners.</span
+<section
+	class="grid grid-cols-1 gap-60 bg-off-black px-4 py-16 text-off-black xl:grid-cols-[1fr_5fr] xl:gap-0"
+>
+	<div class="sticky top-12 flex flex-col gap-0">
+		<div class="flex flex-col gap-10 bg-off-black">
+			<div class="dot-bg row-start-1 row-end-2 pt-8"></div>
+			<div
+				class="row-start-2 row-end-3 flex size-fit items-center rounded-[8px] bg-white px-4 py-3 leading-[1.5rem] font-bold text-nowrap text-off-black xl:text-[1rem] xl:leading-[1.1rem]"
 			>
-		</h6>
-	</div>
-	<div class="section-content -mt-16 w-3/4">
-		{#each useCases as uc}
-			<div>
-				<p class="mb-4 font-[Milling] text-[0.8rem] font-extrabold tracking-wide">{uc.title}</p>
-				<p class="mb-14 text-[1rem] tracking-tight">
-					{uc.content}
-				</p>
+				Real-World Impact and Use Cases
 			</div>
-		{/each}
+			<h6
+				class="row-start-3 row-end-4 w-full text-[1.25rem] leading-[1.625rem] font-semibold text-white sm:text-[1.5rem] sm:leading-[2rem] sm:font-bold"
+			>
+				aiD demonstrates its research progress through more than 10 high-impact use cases organized
+				into five topical areas and driven by industrial partners:
+			</h6>
+		</div>
+		<div
+			class="row-start-4 row-end-5 h-[8rem] w-full bg-linear-to-b from-black to-transparent"
+		></div>
 	</div>
+	{#each useCases as uc}
+		<div>
+			<p class="mb-[0.75rem] font-[Milling] text-[1rem] leading-[1.5rem] font-bold text-white">
+				{uc.title}
+			</p>
+
+			<p class="font-[Montagu_Slab] text-[1rem] leading-[1.5rem] tracking-tight text-white">
+				{uc.content}
+			</p>
+		</div>
+	{/each}
 </section>
 
 <style>
-	.title-area {
-		display: flex;
-		flex-direction: column;
-		gap: 3rem;
-		margin-left: 0.2rem;
-		height: fit-content;
-	}
-	.section-content {
-		color: var(--off-black);
-		display: flex;
-		flex-direction: column;
-		align-items: flex-end;
-		gap: 6rem;
-		font-family: 'Montagu Slab';
-		font-weight: 400;
-		line-height: 1.7rem;
-		color: white;
-	}
-	.section-title {
-		background: white;
-		color: var(--off-black);
-		border-radius: 0.5rem;
-		padding: 0.5rem 1rem;
-	}
 	.dot-bg {
 		background-image: url('/src/lib/assets/diagonal_squares_white.svg');
 		background-repeat: repeat-x;
+		background-size: auto;
 	}
 </style>
