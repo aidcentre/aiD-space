6c207332f13d75d8902c8b1d53f64ab1e5dcb0b3
janealsh <janealshihabi@gmail.com>
2026-03-05 16:49:50 +0100

    make navheader responsive

--- src/lib/ui/NavHeader.svelte
+++ src/lib/ui/NavHeader.svelte
@@ -5,20 +5,20 @@
 </script>
 
 <div
-	class="sticky top-0 z-10 flex h-10 w-full items-center bg-off-black pt-4 pb-4 text-[0.875rem] text-light-grey"
+	class="sticky top-0 z-10 flex h-10 w-full items-center bg-off-black px-3 pt-4 pb-4 text-[0.875rem] text-light-grey"
 >
-	<div class="navbar-start overflow-hidden pl-3">
-		<span class="max-sm:hidden">aiD v{version}</span>
+	<div class="navbar-start overflow-hidden">
+		<span class="max-md:hidden">aiD v{version}</span>
 		<img
 			src={aiDLogo}
 			alt="aiD logo and description, Norwegian Centre on AI for Decisions"
-			class="h-[20px] sm:pl-40"
+			class="h-[20px] md:pl-40"
 		/>
 	</div>
-	<div>
+	<div class="max-md:navbar-end">
 		<BackToAid />
 	</div>
-	<div class="node-text navbar-end pr-3">
+	<div class="navbar-end flex items-center gap-2 pr-3 max-md:hidden">
 		<div class="node-text">
 			<div class="square"></div>
 			<span>node 000{nodeNum}</span>
