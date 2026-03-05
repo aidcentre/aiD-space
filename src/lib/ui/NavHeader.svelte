cf925185b21b328e62984eba46b98c479c7fd64c
janealsh <janealshihabi@gmail.com>
2026-03-05 16:46:17 +0100

    add backtoaid component and use in navheader

--- src/lib/ui/NavHeader.svelte
+++ src/lib/ui/NavHeader.svelte
@@ -1,21 +1,23 @@
 <script lang="ts">
+	import BackToAid from './BackToAid.svelte';
 	import aiDLogo from '/src/lib/assets/aiD_logotype_white_RGB.svg';
 	let { version, nodeNum } = $props();
 </script>
 
-<div class="header-main sticky top-0 items-start pt-4 pb-4 text-xs shadow-sm">
+<div
+	class="sticky top-0 z-10 flex h-10 w-full items-center bg-off-black pt-4 pb-4 text-[0.875rem] text-light-grey"
+>
 	<div class="navbar-start overflow-hidden pl-3">
-		<span>aiD v{version}</span>
+		<span class="max-sm:hidden">aiD v{version}</span>
 		<img
 			src={aiDLogo}
 			alt="aiD logo and description, Norwegian Centre on AI for Decisions"
-			class="max-h-[20px] pl-40"
+			class="h-[20px] sm:pl-40"
 		/>
 	</div>
-	<a href="/" class="navbar-center" style:gap="0.5rem">
-		<div class="back-arrow">←</div>
-		<span>Back to aiD</span>
-	</a>
+	<div>
+		<BackToAid />
+	</div>
 	<div class="node-text navbar-end pr-3">
 		<div class="node-text">
 			<div class="square"></div>
@@ -25,25 +27,6 @@
 </div>
 
 <style>
-	.header-main {
-		display: flex;
-		width: 100%;
-		align-items: center;
-		padding: 0.5rem;
-		color: var(--light-grey);
-		background-color: var(--off-black);
-		height: fit-content;
-		z-index: 10;
-	}
-	.back-arrow {
-		display: flex;
-		padding: 0.25rem;
-		line-height: 0.75rem;
-		justify-content: center;
-		align-items: center;
-		border-radius: 0.25rem;
-		border: 1px solid var(--light-grey, #e8e8e8);
-	}
 	.node-text {
 		display: flex;
 		align-items: center;
