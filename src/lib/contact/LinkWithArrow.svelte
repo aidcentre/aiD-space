b7f5a451561d9526160222675c7a2fca6f6e73af
janealsh <janealshihabi@gmail.com>
2026-03-09 17:13:18 +0100

    make arrow in-line and underlined and dynamic coloring

--- src/lib/contact/LinkWithArrow.svelte
+++ src/lib/contact/LinkWithArrow.svelte
@@ -2,41 +2,38 @@
 	let { label, white = false } = $props();
 
 	// svelte-ignore non_reactive_update
-	let borderColor: string;
+	let textColor: string;
+	let arrowClass: string;
 	// svelte-ignore state_referenced_locally
-	white ? (borderColor = 'border-(--light-grey)') : (borderColor = 'border-off-black');
+	white ? (textColor = 'var(--light-grey)') : (textColor = 'var(--off-black)');
+	white ? (arrowClass = 'link-with-arrow-white') : (arrowClass = 'link-with-arrow-black');
 </script>
 
 <!-- TODO: add prop to pass in nav link once they've been decided -->
 <a
-	class="flex w-fit cursor-pointer flex-nowrap items-center gap-1 border-b border-solid {borderColor} font-[Montagu_Slab] text-[1rem] leading-[1.5rem] font-normal tracking-[0.32px]"
+	class="{arrowClass} w-fit cursor-pointer items-center gap-1 font-[Montagu_Slab] text-[1rem] leading-[1.5rem] font-normal tracking-[0.32px] text-wrap underline underline-offset-4"
 	class:text-(--light-grey)={white}
 	class:text-(--off-black)={!white}
 	href="https://www.forskningsradet.no/en/research-policy-strategy/ltp/artificial-intelligence/"
 	target="_blank"
 	>{label}
-	<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<path
-			xmlns="http://www.w3.org/2000/svg"
-			fill="none"
-			stroke="currentColor"
-			stroke-width="1.5"
-			stroke-linecap="butt"
-			stroke-linejoin="miter"
-			stroke-miterlimit="10"
-			d="M 13.005208 3 L 3.005208 13 "
-			transform="matrix(0.75,0,0,0.75,0,0)"
-		/>
-		<path
-			xmlns="http://www.w3.org/2000/svg"
-			fill="none"
-			stroke="currentColor"
-			stroke-width="1.5"
-			stroke-linecap="butt"
-			stroke-linejoin="miter"
-			stroke-miterlimit="10"
-			d="M 13 9.875 L 13 3 L 6.125 3 "
-			transform="matrix(0.75,0,0,0.75,0,0)"
-		/></svg
-	>
 </a>
+
+<style>
+	.link-with-arrow-black::after {
+		content: url('$lib/assets/arrow_up_right_black.svg');
+		display: inline-flex;
+		align-items: bottom;
+		height: 12px;
+		width: 12px;
+		padding-left: 0.25rem;
+	}
+	.link-with-arrow-white::after {
+		content: url('$lib/assets/arrow_up_right_white.svg');
+		display: inline-flex;
+		align-items: bottom;
+		height: 12px;
+		width: 12px;
+		padding-left: 0.25rem;
+	}
+</style>
