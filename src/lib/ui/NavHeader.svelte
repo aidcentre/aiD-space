5678b5a60219c27179198fb9b8323fb5c3ee0781
janealsh <janealshihabi@gmail.com>
2026-03-10 13:36:18 +0100

    make navheader take up full width

--- src/lib/ui/NavHeader.svelte
+++ src/lib/ui/NavHeader.svelte
@@ -4,26 +4,28 @@
 	let { version, nodeNum } = $props();
 </script>
 
-<div
-	class="sticky top-0 z-10 flex h-10 w-full items-center bg-off-black px-3 pt-4 pb-4 text-[0.875rem] text-light-grey"
->
-	<div class="navbar-start overflow-hidden">
-		<span class="max-md:hidden">aiD v{version}</span>
-		<img
-			src={aiDLogo}
-			alt="aiD logo and description, Norwegian Centre on AI for Decisions"
-			class="h-[20px] md:pl-40"
-		/>
-	</div>
-	<div class="max-md:navbar-end">
-		<BackToAid label="Back to aiD" direction="left" />
-	</div>
-	<div class="navbar-end flex items-center gap-2 pr-3 max-md:hidden">
-		<div class="node-text">
-			<div class="square"></div>
-			<span>node 000{nodeNum}</span>
+<div class="viewport-width bg-off-black">
+	<header
+		class="sticky top-0 z-10 flex h-10 w-full max-w-[1920px] items-center px-3 pt-4 pb-4 text-[0.875rem] text-light-grey"
+	>
+		<div class="navbar-start overflow-hidden">
+			<span class="max-md:hidden">aiD v{version}</span>
+			<img
+				src={aiDLogo}
+				alt="aiD logo and description, Norwegian Centre on AI for Decisions"
+				class="h-[20px] md:pl-40"
+			/>
 		</div>
-	</div>
+		<div class="max-md:navbar-end">
+			<BackToAid label="Back to aiD" direction="left" />
+		</div>
+		<div class="navbar-end flex items-center gap-2 pr-3 max-md:hidden">
+			<div class="node-text">
+				<div class="square"></div>
+				<span>node 000{nodeNum}</span>
+			</div>
+		</div>
+	</header>
 </div>
 
 <style>
