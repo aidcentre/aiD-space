915fd0bfa347395f8194e1558e1c980f9007ba51
janealsh <janealshihabi@gmail.com>
2026-03-10 11:03:50 +0100

    make background of footer span entire viewport but its content fit in wrapper size

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-<main class="page-layout">
+<main class="wrapper">
 	{@render children()}
 </main>
 
@@ -15,8 +15,22 @@
 	:global(html) {
 		background-color: var(--light-grey);
 	}
-	.page-layout {
+	.wrapper {
 		max-width: 1920px;
 		margin: 0 auto;
+	}
+
+	/* css for background items that should not be constrained by 1920px wrapper */
+	/* if using, set background color and use w-max-[1920px] in child element */
+	:global(.viewport-width) {
+		display: flex;
+		justify-content: center;
+		position: relative;
+		right: 50%;
+		left: 50%;
+		margin-left: -50vw;
+		margin-right: -50vw;
+		max-width: 100vw;
+		width: 100vw;
 	}
 </style>
