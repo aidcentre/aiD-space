47a831f76e3af85a7dea74e1dc2d2bba3aa051bd
janealsh <janealshihabi@gmail.com>
2026-03-12 14:00:37 +0100

    preliminary text scramble effect

--- src/lib/actions/Scramble.svelte
+++ src/lib/actions/Scramble.svelte
@@ -2,44 +2,84 @@
 	import { gsap } from 'gsap';
 	import { ScrambleTextPlugin } from 'gsap/ScrambleTextPlugin';
 	import { ScrollTrigger } from 'gsap/ScrollTrigger';
+	import { SplitText } from 'gsap/SplitText';
 	import { onMount } from 'svelte';
-	const { text, class: className = '' } = $props();
+	const { text = '', class: className = '' } = $props();
 
 	// scrolltrigger plugin makes animation run when user scrolls to it
-	gsap.registerPlugin(ScrambleTextPlugin, ScrollTrigger);
+	gsap.registerPlugin(ScrambleTextPlugin, ScrollTrigger, SplitText);
 
 	let element: HTMLElement;
 
 	onMount(() => {
-		// check if span is already in viewport. if not, wait until user scrolls to animate
-		const rect = element.getBoundingClientRect();
-		const isInViewport = rect.top < window.innerHeight && rect.bottom > 0;
+		console.log('the element as-is -->', element);
+		let st = SplitText.create(element, { type: 'chars, words' });
 
-		if (isInViewport) {
-			gsap.to(element, {
-				duration: 1.5,
+		// prevent wrapping mid-word
+		// st.words.forEach((word) => {
+		// 	word.style.display = 'inline-block';
+		// 	word.style.whiteSpace = 'nowrap';
+		// });
+
+		console.log('the split', st);
+
+		const chars = st.chars.filter((char) => char.textContent.trim() !== '');
+
+		console.log('the chars --->', chars);
+
+		chars.forEach((char, i) => {
+			gsap.to(char, {
+				opacity: 1,
+				duration: 0.8,
+				delay: i * 0.008,
 				scrambleText: {
-					text: text,
+					text: char.textContent,
 					chars: '0123456789',
-					speed: 0.5
+					speed: 1
 				}
 			});
-		} else {
-			gsap.to(element, {
-				duration: 1.5,
-				scrambleText: {
-					text: text,
-					chars: '0123456789',
-					speed: 0.5
-				},
-				scrollTrigger: {
-					trigger: element,
-					start: 'bottom 75%',
-					toggleActions: 'restart none none none'
-				}
-			});
-		}
+		});
 	});
+
+	// onMount(() => {
+	// 	// check if span is already in viewport. if not, wait until user scrolls to animate
+	// 	const rect = element.getBoundingClientRect();
+	// 	const isInViewport = rect.top < window.innerHeight && rect.bottom > 0;
+
+	// 	console.log('jus the element', element);
+	// 	console.log('the elements text', element.textContent);
+
+	// 	let st = SplitText.create(element.textContent, { type: 'chars' });
+
+	// 	console.log('the split', st);
+
+	// 	if (isInViewport) {
+	// 		st.chars.forEach((char) => {
+	// 			gsap.to(char, {
+	// 				duration: 1,
+	// 				scrambleText: {
+	// 					text: text,
+	// 					chars: '0123456789',
+	// 					speed: 0.5
+	// 				}
+	// 			});
+	// 		});
+	// 	} else {
+	// 		gsap.to(element, {
+	// 			duration: 1.5,
+	// 			scrambleText: {
+	// 				text: text,
+	// 				chars: '0123456789',
+	// 				speed: 0.5
+	// 			},
+	// 			scrollTrigger: {
+	// 				trigger: element,
+	// 				start: 'bottom 75%',
+	// 				toggleActions: 'restart none none none'
+	// 			}
+	// 		});
+	// 	}
+	// });
 </script>
 
 <span bind:this={element} class="{className} min-h-full w-full">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,8 @@
 	import HomeHeader from '$lib/ui/HomeHeader.svelte';
 	import SearchBar from '$lib/ui/SearchBar.svelte';
 	import Tornado from '$lib/ui/Tornado.svelte';
-	// import { animate } from '$lib/actions/scramble_text';
+	import { animate } from '$lib/actions/scramble_text';
+	import Scramble from '$lib/actions/Scramble.svelte';
 
 	let footerText =
 		"The Norwegian Center on AI for Decisions is building a scientific and technological foundation that makes reliable, real-world AI possible. We\'re building something great here. Watch this space.";
@@ -21,6 +22,6 @@
 	<footer
 		class="w-full self-start px-3 pb-3 text-[1rem] leading-6 font-extrabold wrap-anywhere text-off-black sm:w-2/3 sm:text-[1.5rem] sm:leading-8"
 	>
-		{footerText}
+		<Scramble text={footerText} />
 	</footer>
 </main>
