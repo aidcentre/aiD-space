1c8c618529c45aba98a4e0f1d5ac8d57e17b5f95
Morten Johansen <mortenjohansen@Morten-sin-MacBook-Pro.local>
2026-04-24 12:59:28 +0200

    sanity init

--- src/lib/meta/Meta.svelte
+++ src/lib/meta/Meta.svelte
@@ -5,11 +5,9 @@
 	let title = 'Norwegian Centre on AI for Decisions';
 
 	let meta = $derived({
-		// default values
 		description:
 			'The aiD centre, or the Norwegian Centre on AI for Decisions, is an initiative led by the Research Council of Norway, SINTEF, and NTNU.',
 		title,
-		// per-page customizations, defined in the +page.ts files
 		...page.data.meta
 	});
 
@@ -21,8 +19,6 @@
 <svelte:head>
 	<title>{titleDecoration(meta.title)}</title>
 	<meta name="description" content={meta.description} />
-	<!-- many links (e.g. https://aid-centre.no, https://www.aid-centre.no, https://aid-center.no) can access the content of this set. Below, we set the canonical url.-->
 	<link rel="canonical" href={`${PUBLIC_BASE_URL}${page.url.pathname}`} />
-	<!-- for SEO, add an apple touch icon -->
 	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 </svelte:head>
